abb1ad1e1ee55f30a463b7a73ef115488bc4e264
Itallo <italloazevedo@hotmail.com>
2020-04-08 14:40:20 -0300

    fix: dados atualizados com o boletim da sespa

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -8,14 +8,15 @@ insert into boletim values(7, '2020-03-25', 2, 0, true, 7, 0);
 insert into boletim values(8, '2020-03-26', 6, 0, true, 13, 0);
 insert into boletim values(9, '2020-03-27', 0, 0, true, 13, 0);
 insert into boletim values(10, '2020-03-28', 4, 0, true, 17, 0);
-insert into boletim values(11, '2020-03-29', 1, 0, true, 18, 0);
-insert into boletim values(12, '2020-03-30', 3, 0, true, 21, 0);
-insert into boletim values(13, '2020-03-31', 11, 0, true, 32, 0);
-insert into boletim values(14, '2020-04-01', 8, 1, true, 40, 1);
-insert into boletim values(15, '2020-04-02', 6, 0, true, 46, 1);
-insert into boletim values(16, '2020-04-03', 4, 0, true, 50, 1);
-insert into boletim values(17, '2020-04-04', 30, 0, true, 80, 1);
-insert into boletim values(18, '2020-04-05', 6, 0, true, 86, 1);
-insert into boletim values(19, '2020-04-06', 16, 2, true, 102, 3);
-insert into boletim values(20, '2020-04-07', 36, 2, true, 138, 5);
+insert into boletim values(11, '2020-03-29', 2, 0, true, 19, 0);
+insert into boletim values(12, '2020-03-30', 7, 0, true, 26, 0);
+insert into boletim values(13, '2020-03-31', 8, 0, true, 34, 0);
+insert into boletim values(14, '2020-04-01', 7, 1, true, 41, 1);
+insert into boletim values(15, '2020-04-02', 7, 0, true, 48, 1);
+insert into boletim values(16, '2020-04-03', 27, 0, true, 75, 1);
+insert into boletim values(17, '2020-04-04', 7, 0, true, 82, 1);
+insert into boletim values(18, '2020-04-05', 4, 0, true, 86, 1);
+insert into boletim values(19, '2020-04-06', 37, 3, true, 123, 4);
+insert into boletim values(20, '2020-04-07', 31, 1, true, 154, 5);
+
 
