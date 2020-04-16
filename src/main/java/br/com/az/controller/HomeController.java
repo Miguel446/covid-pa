a885950cd0b953bb14eb1db6be2d5cfbcf0e1adc
Itallo <italloazevedo@hotmail.com>
2020-04-16 11:41:19 -0300

    fix: headers adicionados para Api ds

--- src/main/java/br/com/az/controller/HomeController.java
+++ src/main/java/br/com/az/controller/HomeController.java
@@ -3,10 +3,11 @@ package br.com.az.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import br.com.az.model.Boletim;
 import br.com.az.service.BoletimService;
@@ -22,10 +23,20 @@ public class HomeController {
 		return "index";
 	}
 
+	/*
+	 * @GetMapping("/boletim")
+	 * 
+	 * @ResponseBody public List<Boletim> listar() { return
+	 * boletimService.listarTodos(); }
+	 */
+
 	@GetMapping("/boletim")
-	@ResponseBody
-	public List<Boletim> listar() {
-		return boletimService.listarTodos();
+	public ResponseEntity<List<Boletim>> boletim() {
+		HttpHeaders responseHeaders = new HttpHeaders();
+		responseHeaders.set("access-control-allow-credentials", "true");
+		responseHeaders.set("access-control-allow-origin", "*");
+
+		return ResponseEntity.ok().headers(responseHeaders).body(boletimService.listarTodos());
 	}
 
 }
