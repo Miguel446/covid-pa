b0dad1fcde3b22c995e023bff0b598fa8ea8373c
Itallo <italloazevedo@hotmail.com>
2020-05-12 19:41:06 -0300

    dados atualizados 12/05

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -53,4 +53,5 @@ insert into boletim values(51, '2020-05-08', 584, 86, 0,  true, 6519, 574, 98);
 insert into boletim values(52, '2020-05-09', 499, 36, 0,  true, 7018, 610, 98);
 insert into boletim values(53, '2020-05-10', 330, 62, 0,  true, 7348, 672, 98);
 insert into boletim values(54, '2020-05-11', 721, 140, 0,  true, 8069, 812, 98);
+insert into boletim values(55, '2020-05-12', 990, 102, 0,  true, 9059, 914, 98);
 
